4293a70631dcd2300b81d4eff1b54c4ec16842e0
Ethaniel Botello <Ebotel200@west-mec.local>
2021-01-25 15:25:14 -0700

    update

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
 
-let passenger = new Object()
+let passenger = new Object();
 
-passenger.firstName = 'Fred'
+let filled = 0;
+let meal = 'None'
+let extra = 'None'
 
-let hello = passenger.firstName;
+document.getElementById("firstName").value = "boi"
+document.getElementById("lastName").value = "boi2"
 
-console.log(`first name is ${hello}`);
 
-function print(){
-    let name = document.getElementById("dOB").value;
-    document.getElementById("test").innerHTML = (`You were born on ${name}`)
+function meal1(){
+    meal = "Chicken"
+    document.getElementById("test").innerHTML = ("chimken")
 }
+function meal2(){
+    meal = "Fish"
+    document.getElementById("test").innerHTML = ("fyshe")
+}
+function meal3(){
+    meal = "Veggies"
+    document.getElementById("test").innerHTML = ("veggy")
+}
+
+function extra1(){
+    extra = "Legroom"
+}
+function extra2(){
+    extra = "Window Seat"
+}
+function extra3(){
+    extra = "Headphones"
+}
+function extra4(){
+    extra = "Extra Food"
+}
+
+
+passenger.name = document.getElementById("firstName").value;
+function print(){
+    passenger.name = document.getElementById("firstName").value;
+    document.getElementById("test").innerHTML = (`${passenger.name}`);
+    console.log(`${passenger.firstName}`);
+}
+
+
+
+passenger.firstName = document.getElementById("firstName").value;;
+passenger.lastName = '';
+passenger.dOB = '';
+passenger.leavingDate = '';
+passenger.goingDate = '';
+passenger.bags = '';
+passenger.meal = `${meal}`;
+passenger.extra = `${extra}`;
